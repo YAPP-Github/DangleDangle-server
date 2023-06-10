39abef2421ff64f24c67d3bca47b8c825518f766
algosipdahack <sophia5460@ewhain.net>
2023-06-10 18:18:08 +0900

    hotfix : build error

---
+++ src/domain/observationAnimal/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    compileOnly("org.springframework:spring-tx")
+    compileOnly("org.springframework:spring-context")
+    compileOnly(project(":common"))
+}
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
