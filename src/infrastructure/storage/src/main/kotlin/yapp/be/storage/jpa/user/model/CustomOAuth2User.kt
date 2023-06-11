825a4b0994f71a4ec4c0a2d33645f181ddd05743
algosipdahack <sophia5460@ewhain.net>
2023-06-12 02:11:53 +0900

    [feat/YW2-18] OAuth2.0 Config 설정

--- src/infrastructure/storage/src/main/kotlin/yapp/be/storage/jpa/user/model/CustomOAuth2User.kt
+++ src/infrastructure/storage/src/main/kotlin/yapp/be/storage/jpa/user/model/CustomOAuth2User.kt
@@ -3,9 +3,9 @@ package yapp.be.storage.jpa.user.model
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 
+
 class CustomOAuth2User(
-    private val authorities: Collection<GrantedAuthority?>?,
-    private val attributes: Map<String, Any?>,
-    private val nameAttributeKey: String?,
-    val oAuthAttributes: OAuthAttributes
+    authorities: Collection<GrantedAuthority?>?,
+    attributes: Map<String, Any?>, nameAttributeKey: String?,
+    private val oAuthAttributes: OAuthAttributes
 ) : DefaultOAuth2User(authorities, attributes, nameAttributeKey)
--- src/infrastructure/storage/src/main/kotlin/yapp/be/storage/jpa/user/model/OAuthAttributes.kt
+++ src/infrastructure/storage/src/main/kotlin/yapp/be/storage/jpa/user/model/OAuthAttributes.kt
@@ -1,6 +1,18 @@
 package yapp.be.storage.jpa.user.model
 
+import yapp.be.enum.OAuthType
+
 class OAuthAttributes(val attributes: Map<String, Any>, val nameAttributeKey: String, val email: String) {
+    fun toEntity(): UserEntity {
+        return UserEntity(
+            email = email,
+            nickname = "",
+            phone = "",
+            oAuthType = OAuthType.KAKAO,
+            oAuthAccessToken = "",
+            shelterId = ""
+        )
+    }
     companion object {
         fun of(
             attributes: Map<String, Any>
