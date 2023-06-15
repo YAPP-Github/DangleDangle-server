e6b8ea3e6ae69c0e01a1bd3c81905cfe21031664
ktj1997 <rlaxowns7916@gmail.com>
2023-06-15 20:57:24 +0900

    [YW2-19] feat: Swagger Setting

--- src/app/api/src/main/kotlin/yapp/be/apiapplication/shelter/controller/ShelterSignUpController.kt
+++ src/app/api/src/main/kotlin/yapp/be/apiapplication/shelter/controller/ShelterSignUpController.kt
@@ -1,50 +1,33 @@
 package yapp.be.apiapplication.shelter.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import yapp.be.apiapplication.shelter.controller.model.SignUpWithAdditionalInfoRequest
 import yapp.be.apiapplication.shelter.controller.model.SignUpWithEssentialInfoRequest
 import yapp.be.apiapplication.shelter.service.ShelterSignUpApplicationService
-import yapp.be.apiapplication.shelter.service.model.SignUpWithAdditionalInfoResponseDto
 import yapp.be.apiapplication.shelter.service.model.SignUpWithEssentialInfoResponseDto
 
 @RestController
-@RequestMapping("/v1/shelter/user")
-class ShelterUserSignUpController(
+@Tag(name = "보호소 회원가입 api")
+@RequestMapping("/v1/shelter")
+class ShelterSignUpController(
     val shelterSignUpApplicationService: ShelterSignUpApplicationService
 ) {
-
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/sign-up/essential-info")
+    @PostMapping("/sign-up")
     @Operation(
-        summary = "보호소 사용자 회원가입 (필수정보)",
+        summary = "보호소 사용자 회원가입",
     )
     fun signUpWithEssentialInfo(
         req: SignUpWithEssentialInfoRequest
     ): ResponseEntity<SignUpWithEssentialInfoResponseDto> {
         val reqDto = req.toDto()
         val resDto = shelterSignUpApplicationService.signUpWithEssentialInfo(reqDto)
-
-        return ResponseEntity.ok(resDto)
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/sign-up/additional-info")
-    @Operation(
-        summary = "보호소 사용자 회원가입 (추가정보)",
-    )
-    fun signUpWithAdditionalInfo(
-        req: SignUpWithAdditionalInfoRequest
-    ): ResponseEntity<SignUpWithAdditionalInfoResponseDto> {
-        val reqDto = req.toDto()
-        val resDto = shelterSignUpApplicationService.signUpWithAdditionalInfo(
-            reqDto = reqDto
-        )
 
         return ResponseEntity.ok(resDto)
     }
