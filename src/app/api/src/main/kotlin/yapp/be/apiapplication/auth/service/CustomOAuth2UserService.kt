b3cfce0fcc8e09e4d7b7602c715b429e6b99d060
algosipdahack <sophia5460@ewhain.net>
2023-06-17 19:35:51 +0900

    [YW2-18] 유저 검증 및 닉네임 중복 체크

--- src/app/api/src/main/kotlin/yapp/be/apiapplication/auth/service/CustomOAuth2UserService.kt
+++ src/app/api/src/main/kotlin/yapp/be/apiapplication/auth/service/CustomOAuth2UserService.kt
@@ -7,27 +7,17 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
-import yapp.be.domain.service.CheckUserService
 import yapp.be.enum.Role
-import yapp.be.exceptions.CustomException
-import yapp.be.storage.config.exceptions.StorageExceptionType
 import yapp.be.storage.jpa.user.model.CustomOAuth2User
 import yapp.be.storage.jpa.user.model.OAuthAttributes
 
 @Service
-class CustomOAuth2UserService(
-    private val checkUserService: CheckUserService
-) : OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+class CustomOAuth2UserService : OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     @Throws(OAuth2AuthenticationException::class)
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
         val delegate = DefaultOAuth2UserService()
         val oAuth2User = delegate.loadUser(userRequest)
         val attributes: OAuthAttributes = OAuthAttributes.of(oAuth2User.attributes)
-
-        if (!checkUserService.isExist(attributes.email)) {
-            throw CustomException(StorageExceptionType.ENTITY_NOT_FOUND, "해당 사용자가 존재하지 않습니다.")
-        }
-
         return CustomOAuth2User(
             setOf(SimpleGrantedAuthority(Role.VOLUNTEER.name)),
             attributes.attributes,
