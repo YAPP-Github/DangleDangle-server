fde8a04740ef4d249d29af05c90944f455462a08
algosipdahack <sophia5460@ewhain.net>
2023-06-19 18:38:02 +0900

    [YW2-18] JPA Auditing 오류 수정

--- src/app/api/src/main/kotlin/yapp/be/ApiApplication.kt
+++ src/app/api/src/main/kotlin/yapp/be/ApiApplication.kt
@@ -3,8 +3,11 @@ package yapp.be
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableJpaAuditing
 class ApiApplication
 
 fun main(args: Array<String>) {
