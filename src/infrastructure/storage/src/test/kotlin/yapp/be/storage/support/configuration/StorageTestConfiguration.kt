a7382c016b956bc0c3eaa7fa414c3590b439c82e
algosipdahack <sophia5460@ewhain.net>
2023-06-25 16:08:19 +0900

    [YW2-79] feat/ci-cd jib 스크립트 생성

--- src/infrastructure/storage/src/test/kotlin/yapp/be/storage/support/configuration/StorageTestConfiguration.kt
+++ src/infrastructure/storage/src/test/kotlin/yapp/be/storage/support/configuration/StorageTestConfiguration.kt
@@ -5,6 +5,6 @@ import org.springframework.test.context.ContextConfiguration
 import yapp.be.storage.config.JpaConfig
 import yapp.be.storage.config.QueryDslConfig
 
-@SpringBootApplication(scanBasePackages = ["yapp.be"])
+@SpringBootApplication(scanBasePackages = ["yapp.be.storage"])
 @ContextConfiguration(classes = [JpaConfig::class, QueryDslConfig::class])
 class StorageTestConfiguration
