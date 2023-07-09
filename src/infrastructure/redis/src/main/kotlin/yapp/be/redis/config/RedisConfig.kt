ef882684c3d0c9c30ac2d58ac972bfb29b5a6259
ktj1997 <rlaxowns7916@gmail.com>
2023-07-09 16:30:07 +0900

    fix: test properties

--- src/infrastructure/redis/src/main/kotlin/yapp/be/redis/config/RedisConfig.kt
+++ src/infrastructure/redis/src/main/kotlin/yapp/be/redis/config/RedisConfig.kt
@@ -1,6 +1,5 @@
 package yapp.be.redis.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -8,14 +7,7 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-data class RedisConfig(
-    @Value("\${spring.data.redis.host}")
-    private val host: String,
-    @Value("\${spring.data.redis.port}")
-    private val port: Int,
-    @Value("\${spring.data.redis.password}")
-    val password: String?
-) {
+class RedisConfig {
 
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
