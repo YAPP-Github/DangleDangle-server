8319dfcdbc69e41ffbb92a5d14e0a5aac0e8495d
ktj1997 <rlaxowns7916@gmail.com>
2023-07-18 15:53:29 +0900

    fix: Validation String -> Const

--- src/app/api/src/main/kotlin/yapp/be/apiapplication/auth/controller/model/LoginShelterUserRequest.kt
+++ src/app/api/src/main/kotlin/yapp/be/apiapplication/auth/controller/model/LoginShelterUserRequest.kt
@@ -2,13 +2,15 @@ package yapp.be.apiapplication.auth.controller.model
 
 import jakarta.validation.constraints.Pattern
 import yapp.be.apiapplication.auth.service.model.LoginShelterUserRequestDto
+import yapp.be.model.support.EMAIL_REGEX
+import yapp.be.model.support.PASSWORD_REGEX
 import yapp.be.model.vo.Email
 
 data class LoginShelterUserRequest(
-    @field:Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9-.]+\$", message = "잘못된 이메일 형식입니다.")
+    @field:Pattern(regexp = EMAIL_REGEX, message = "잘못된 이메일 형식입니다.")
     val email: String,
     @field:Pattern(
-        regexp = "^(?!((?:[A-Za-z]+)|(?:[~!@#\$%^&*()_+=]+)|(?:[0-9]+))\$)[A-Za-z\\d~!@#\$%^&*()_+=]{8,15}\$",
+        regexp = PASSWORD_REGEX,
         message = "비밀번호는 영문, 숫자, 특수문자 2가지 조합 8~15자이어야 합니다."
     )
     val password: String
